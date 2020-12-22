11dbcbf77c9248b1e509e4de4d1edc85a1f1b512
Yogesh Kumar <yogeshykk5@gmail.com>
2020-12-22 18:28:51 +0530

    First Commit

---
+++ src/com/sample/test/Test1.java
@@ -0,0 +1,12 @@
+package com.sample.test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println("Sum = "+c);
+	}
+}
